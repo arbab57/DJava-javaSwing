908937a16ea671b05af87a22bc0e9b3b13f6eb50
Arbab Bashir <arbabbashir30@gamil.com>
2025-02-10 19:44:20 +0500

    fixed the launcher

--- DJava.cpp
+++ DJava.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <windows.h>
+#include <shlwapi.h>
+#include <filesystem>
 
 int main() {
-    std::string javaProgramPath = "C:\\Users\\arbab84\\java\\DJava";
+    char path[MAX_PATH];
+    if (GetModuleFileNameA(NULL, path, MAX_PATH)) {
+    std::string javaProgramPath = std::filesystem::path(path).parent_path().string();
     std::string command = "cd " + javaProgramPath + " && java Main && exit";
     HWND window;
     AllocConsole();
     window = FindWindowA("ConsoleWindowClass", NULL);
     ShowWindow(window, 0);
     system(command.c_str());
+    } else {
+        std::cerr << "Error retrieving launch directory." << std::endl;
+        system("pause");
+    }
+
+
+
+
+
     return 0;
 }
